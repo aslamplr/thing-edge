425b62d7f519d4f84328a27129318ec1d3ae5940
Aslam Ahammed <aslamplr@gmail.com>
2016-08-07 17:23:12 +0530

    revert last changes

--- cli/espcmd.js
+++ cli/espcmd.js
@@ -1,8 +1,7 @@
 const config = require("../.config.json"),
       mqtt = require("mqtt"),
       client = mqtt.connect(config.mqtt),
-      WebSocket = require('ws'),
-      ws = new WebSocket(config.wss);
+      WebSocket = require('ws');
 
 const status_topic = 'esp8266/status/led',
       control_topic = 'esp8266/control/led';
@@ -13,8 +12,15 @@ function command_esp(message){
     console.log("Sending message to esp %s %s", control_topic, message);
     client.publish(control_topic, message);
     client.end();
-    ws.send(JSON.stringify({action: 'post', payload: message[0] == '1'}));
-    ws.close();
+    let ws;
+    try {
+      ws = new WebSocket(config.wss);
+      ws.send(JSON.stringify({action: 'post', payload: message[0] == '1'}));
+      ws.close();
+    } catch (e) {
+      if(ws) ws.close();
+      console.log("Server seems to be down, no socket connection", e);
+    }
 }
 
 setTimeout(()=> { command_esp(arg_msg); }, 100);
